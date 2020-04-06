3d8f9a28fe20cd9800fbe6a890a7ce2a959388fa
dolev zoozel <dolev461@gmail.com>
2020-04-06 19:28:21 +0300

    Add group feature works

--- bot_manager.py
+++ bot_manager.py
@@ -3,6 +3,10 @@ import config
 import db
 
 
+class UserAlreadyExistsError(Exception):
+    pass
+
+
 class UserNotExistError(Exception):
     pass
 
@@ -22,19 +26,29 @@ class BotManager(object):
 
     def __init__(self):
         self.bot = telebot.TeleBot(config.config['API_TOKEN'])
-        self._user_dict = {}
+        # Save user details before adding it for external use
+        self.pending_user = {
+            "name": None,
+            "phone": None
+        }
         self._commands = {
-            "/start": "Help",
-            "/help": "Help",
-            "/info": "Help"
+            "/start": "Register your telegram account to the bot",
+            "/help": "Show this help section",
+            "/info": "Get your balance in your associated groups"
+        }
+        self._group_admin_commands = {
+            "/add": "Group admin command to add members",
+            "/rm": "Group admin command to remove members",
+            "/members": "Group admin command to display all group members"
         }
         self._admin_commands = {
-            "/add": "Help",
-            "/del": "Help"
+            "/groupadd": "Admin command to create a new group",
+            "/grouprm": "Admin command to remove a group"
         }
 
     @staticmethod
     def format_il_phone_number(phone):
+        phone = phone.replace("-", "")
         if phone.startswith('+972'):
             return phone
         if phone.startswith('972'):
@@ -51,24 +65,64 @@ class BotManager(object):
         except db.UserNotExistError:
             raise UserNotExistError()
 
-    def add_user(self, chat_id, phone_number):
-        try:
-            db.DBUser(chat_id, phone=BotManager.format_il_phone_number(phone_number), create=True)
-        except db.UserNotExistError:
-            raise UserNotInvited()
+    def add_user(self, chat_id, phone_number, name=None):
+        if name is not None:
+            try:
+                db.DBUser.create_pending_user(phone_number, name)
+            except db.UserAlreadyExistsError:
+                raise UserAlreadyExistsError()
+        else:
+            try:
+                db.DBUser(
+                    chat_id,
+                    phone=BotManager.format_il_phone_number(phone_number),
+                    create=True)
+            except db.UserNotExistError:
+                raise UserNotInvited()
+
+    def get_admin_groups(self, chat_id):
+        user = self.get_user(chat_id)
+        admin_groups = []
+        for group in user.groups():
+            user_phone = BotManager.format_il_phone_number(
+                user.data()['phone'])
+            group_admin_phone = BotManager.format_il_phone_number(
+                group['admin'])
+            if user_phone == group_admin_phone:
+                admin_groups.append(db.Group(group['name']))
+
+        return admin_groups
 
     def get_help(self, chat_id):
         commands = self._commands
 
         try:
             user = self.get_user(chat_id)
-            print("Users: {}".format(user.groups()))
+            if self.get_admin_groups(chat_id):
+                commands.update(self._group_admin_commands)
+
+            if user.data()['admin']:
+                commands.update(self._admin_commands)
+
         except UserNotExistError:
             pass
 
         return "\n".join(
             ["{}: {}".format(cmd, desc)
              for cmd, desc in commands.items()])
+
+    def get_user_balances(self, chat_id):
+        user = self.get_user(chat_id)
+        groups = user.groups()
+
+        balances = {}
+        for group_dict in groups:
+            g_name = group_dict['name']
+            group = db.Group(g_name)
+            balance = group.get_user_balance(chat_id)
+            balances[g_name] = -balance
+
+        return balances
 
     def run(self):
         self.bot.enable_save_next_step_handlers(
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from telebot.types import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
-import pprint
 import logging
 import bot_manager
 
@@ -12,15 +11,19 @@ def send_help(message):
     chat_id = message.chat.id
     bot.send_message(chat_id, manager.get_help(chat_id))
 
+################
+# Subscription #
+################
 
 # Handle '/start'
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def start_subscription(message):
     chat_id = message.chat.id
     bot.send_message(chat_id, manager.WELCOME_MSG)
+
     try:
         manager.get_user(chat_id)
-        end_session(message, chat_id)
+        end_subscription(message, chat_id)
     except bot_manager.UserNotExistError:
         markup = ReplyKeyboardMarkup(row_width=1)
         markup.add(
@@ -35,40 +38,138 @@ def send_welcome(message):
 
 
 def process_phone_number(message):
-    try:
-        chat_id = message.chat.id
-        if message.contact is None:
-            bot.send_message(
-                chat_id,
-                "אי אפשר להמשיך ככה... זה לא אני זה אתה!")
-            # Try again
-            bot.register_next_step_handler(message, process_phone_number)
-            return
-
-        try:
-            manager.add_user(chat_id, message.contact.phone_number)
-            end_session(message, chat_id)
-        except bot_manager.UserNotInvited:
-            bot.send_message(
-                chat_id,
-                "לא הוזמנת :(\nהאחראי פורומים זה הכתובת שלך")
-
-    except Exception as e:
-        bot.reply_to(message, "אוי לא " + str(e))
-
-
-def end_session(message, chat_id):
-    try:
-        user = manager.get_user(chat_id)
-        markup = ReplyKeyboardRemove(selective=False)
+    chat_id = message.chat.id
+    if message.contact is None:
         bot.send_message(
             chat_id,
-            "הסריקה הושלמה! :)\n{}".format(pprint.pformat(user.data())),
-            reply_markup=markup)
+            "אי אפשר להמשיך ככה... זה לא אני זה אתה!")
+        # Try again
+        bot.register_next_step_handler(message, process_phone_number)
+        return
 
-        # TODO: Send value
-    except Exception as e:
-        bot.reply_to(message, "אוי לא " + str(e))
+    try:
+        manager.add_user(chat_id, message.contact.phone_number)
+        end_subscription(message, chat_id)
+    except bot_manager.UserNotInvited:
+        bot.send_message(
+            chat_id,
+            "לא הוזמנת :(\nהאחראי פורומים זה הכתובת שלך")
+
+
+def end_subscription(message, chat_id):
+    user = manager.get_user(chat_id)
+    markup = ReplyKeyboardRemove(selective=False)
+    bot.send_message(
+        chat_id,
+        "הסריקה הושלמה! :)\nשלום {}!".format(user.data()['name']),
+        reply_markup=markup)
+
+
+############
+# Commands #
+############
+
+# Handle '/info'
+@bot.message_handler(commands=['info'])
+def send_info(message):
+    chat_id = message.chat.id
+    balances = manager.get_user_balances(chat_id)
+    balances_str_list = []
+    for group, balance in balances.items():
+        if balance > 0:
+            balances_str_list.append(
+                "קבוצה: {}\n"
+                "כל הכבוד! יש לך עודף של {} שקלים".format(group, balance))
+        elif balance < 0:
+            balances_str_list.append(
+                "קבוצה: {}\n"
+                "יש לך חוב בקופה של {} שקלים".format(group, -balance)
+            )
+        else:
+            balances_str_list.append(
+                "קבוצה: {}\n"
+                "המאזן שלך מושלם!".format(group)
+            )
+
+    balances_str = "\n\n".join(balances_str_list)
+    bot.send_message(chat_id, balances_str)
+
+
+# Handle '/add'
+@bot.message_handler(commands=['add'])
+def start_add_group_user(message):
+    chat_id = message.chat.id
+    admin_groups = manager.get_admin_groups(chat_id)
+    if not admin_groups:
+        return
+
+    markup = ReplyKeyboardMarkup(row_width=1)
+    buttons = []
+    for group in admin_groups:
+        buttons.append(KeyboardButton(group.data()['name']))
+
+    markup.add(*buttons)
+    bot.send_message(
+        chat_id,
+        "לאיפה להוסיף את הבחור/ה החדש/ה?",
+        reply_markup=markup)
+
+    bot.register_next_step_handler(message, process_group_choice)
+
+
+def process_group_choice(message):
+    chat_id = message.chat.id
+    admin_groups_names = [group.data()['name']
+                          for group in manager.get_admin_groups(chat_id)]
+
+    markup = ReplyKeyboardRemove(selective=False)
+    if message.text not in admin_groups_names:
+        bot.send_message(
+            chat_id,
+            "את/ה לא המנהל/ת של הקבוצה!",
+            reply_markup=markup)
+        return
+
+    bot.send_message(
+        chat_id,
+        "מה השם של הבחור/ה החדש/ה?\nניתן לשתף איש קשר ;)",
+        reply_markup=markup)
+    bot.register_next_step_handler(message, process_member_name)
+
+
+def process_member_name(message):
+    chat_id = message.chat.id
+    if message.contact is not None:
+        manager.pending_user["name"] = "{} {}".format(str(message.contact.first_name),
+                                                      str(message.contact.last_name)).strip()
+        manager.pending_user["phone"] = message.contact.phone_number
+        end_add_group_user(chat_id)
+    else:
+        manager.pending_user["name"] = message.text
+        bot.send_message(chat_id, "אפשר את המספר שלו/שלה? ;)")
+        bot.register_next_step_handler(message, process_member_phone)
+
+
+def process_member_phone(message):
+    chat_id = message.chat.id
+    manager.pending_user["phone"] = message.text
+    end_add_group_user(chat_id)
+
+
+def end_add_group_user(chat_id):
+    if (manager.pending_user["name"] is None or
+            manager.pending_user["phone"] is None):
+        return
+
+    try:
+        manager.add_user(
+            chat_id,
+            manager.pending_user["phone"],
+            manager.pending_user["name"])
+    except bot_manager.UserAlreadyExistsError:
+        bot.send_message(chat_id, "המספר טלפון כבר קיים במערכת")
+
+    bot.send_message(chat_id, "תודה!")
 
 
 def main():
